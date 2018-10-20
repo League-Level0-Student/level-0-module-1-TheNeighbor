842c46bdb84505694f8bd3081962d8f69cd56764
league <league@wts22.attlocal.net>
2018-10-20 14:28:47 -0700

    Module 1 Finish

---
+++ src/BadgerSong.java
@@ -0,0 +1,28 @@
+
+public class BadgerSong {
+	public static void main(String[] args) {
+		String b = "Badger, ";
+		String m = "Mushroom, ";
+		String s = "Snake, ";
+		for (int i = 0; i < 3; i++) {
+
+			for (int j = 0; j < 12; j++) {
+				System.out.print(b);
+			}
+
+			System.out.print(m);
+			System.out.println(m);
+
+		}
+		for (int i = 0; i < 1; i++) {
+
+			for (int k = 0; k < 12; k++) {
+				System.out.print(b);
+			}
+			for (int l = 0; l < 5; l++) {
+				System.out.print(s);
+			}
+
+		}
+	}
+}
---
+++ src/Banana_Split/Banana_Split.pde
@@ -0,0 +1,6 @@
+String Ice = "Ice Cream";
+background(#030000);
+text(Ice, 10, 10);
+text(Ice, 10, 20);
+text(Ice, 10, 30);
+text("Banana", 10, 40);
